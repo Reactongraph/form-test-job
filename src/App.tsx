10a11f0ed616eb68e08a79dd50b1bf73c8c1b71c
ReactOngraph <js.developer@ongraph.com>
2023-11-23 20:11:59 +0530

    File updates

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./components/Home";
-import AddForm from "./components/form";
+import AddForm from "./components/Form";
 import { UserProvider } from "./context/UserContext";
 
 const App: React.FC = () => {
